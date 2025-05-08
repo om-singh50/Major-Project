307ad48fe70bd1fbf2daa913d2c7b7bcba18bb6e
Om Singh <130607222+om-singh50@users.noreply.github.com>
2025-05-09 01:39:36 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api', // Change this in production
+  baseURL: 'https://major-project-backend-okz9.onrender.com/api', // Change this in production
 });
 
 export default instance;
