d68a10bd822bbc39bfca10e4cd5829d2780ad51a
Om Singh <130607222+om-singh50@users.noreply.github.com>
2025-05-08 23:15:16 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 // src/components/Footer.jsx
 const Footer = () => (
     <footer className="bg-[#006A71] text-white p-2 text-center text-sm mt-4 shadow-md">
-      © 2025 | Smart Notice Board by Om Kumar Singh and Shagun Singh under the guidance of Dr. Arun Kumar G. (HOD EC JSSATE Noida).
+      © 2025 Smart Notice Board | Final Year Project by Om Kumar Singh and Shagun Singh under the guidance of Dr. Arun Kumar G. (HOD ECE, JSSATE Noida).
     </footer>
   );
   export default Footer;
