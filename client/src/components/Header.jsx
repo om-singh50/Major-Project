5a8a08a714b56e6942feed1325d1556cb82a92f3
Om Singh <130607222+om-singh50@users.noreply.github.com>
2025-05-09 00:21:33 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,7 @@
 // src/components/Header.jsx
 const Header = () => (
     <header className="bg-[#006A71] text-white p-4 text-center text-xl font-semibold shadow-md">
-      SMART REAL-TIME IoT-BASED NOTICE BOARD WITH ROLE-BASED ACCESS CONTROL
+      SMART IoT-BASED NOTICE BOARD WITH ROLE-BASED ACCESS CONTROL
     </header>
   );
   export default Header;
